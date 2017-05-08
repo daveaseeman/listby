4ed64647938ec244436d5ff2c9860b604ff52fec
Dave Seeman <daveseeman@icloud.com>
2017-05-07 22:02:32 -0500

    initial commit

---
+++ mainapp.py
@@ -0,0 +1,64 @@
+import os
+from pocketapp import get_request_token, get_auth_url, get_access_token
+from pocketapp import get_list
+from flask import Flask, render_template, session, request
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+consumer_key = os.environ['POCKET_SECRET']
+app_base_url = 'http://0.0.0.0:5000'
+redirect = '/account'
+redirect_uri = app_base_url + redirect
+
+
+@app.route('/')
+def index():
+    request_token = get_request_token(consumer_key, redirect_uri)
+    session['request_token'] = request_token
+    auth_url = get_auth_url(request_token, redirect_uri)
+    return render_template('index.html', auth_url=auth_url)
+
+
+@app.route(redirect)
+def account():
+    if 'user_name' in session:
+        user_name = session['user_name']
+        return render_template('account.html', user_name=user_name)
+    request_token = session['request_token']
+    user_info = get_access_token(consumer_key, request_token)
+    access_token = user_info['access_token']
+    user_name = user_info['user_name']
+    session['access_token'] = access_token
+    session['user_name'] = user_name
+    return render_template('account.html', user_name=user_name)
+
+
+@app.route('/list')
+def list():
+    access_token = session['access_token']
+    user_name = session['user_name']
+    count = "5"
+    tag = request.values.get('tag')
+    list = get_list(consumer_key, access_token, count, tag)
+    new_list = []
+    for item in list:
+        title = list[item]['resolved_title'].encode('utf-8')
+        print(title)
+        new_list += title
+    return render_template('list.html', tag=tag, list=list, user_name=user_name)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_name', None)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
---
+++ pocketapp.py
@@ -0,0 +1,58 @@
+import requests
+# import webbrowser
+# import time
+# import urllib
+# from uuid import uuid4
+import re
+import json
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+# consumer_key = os.environ['POCKET_SECRET']
+# redirect_uri = 'http://daveaseeman.github.io'
+
+
+def get_request_token(consumer_key, redirect_uri):
+    request_url = 'https://getpocket.com/v3/oauth/request'
+    request_token = requests.post(request_url,
+                                  data={'consumer_key': consumer_key,
+                                        'redirect_uri': redirect_uri}
+                                  )
+    request_token = request_token.text.replace('code=', '')
+    return request_token
+
+
+def get_auth_url(request_token, redirect_uri):
+    auth_base_url = 'https://getpocket.com/auth/authorize'
+    token_param = '?request_token=' + request_token
+    redirect_param = '&redirect_uri=' + redirect_uri
+    auth_url = auth_base_url + token_param + redirect_param
+    return auth_url
+
+
+def get_access_token(consumer_key, request_token):
+    access_url = 'https://getpocket.com/v3/oauth/authorize'
+    access_response = requests.post(access_url,
+                                    data={'consumer_key': consumer_key,
+                                          'code': request_token}
+                                    )
+    response = re.split(r'[=&]', access_response.text)
+    user_info = {'access_token': response[1], 'user_name': response[3]}
+    return user_info
+
+
+def get_list(consumer_key, access_token, count, tag):
+    get_url = 'https://getpocket.com/v3/get'
+    response = requests.post(get_url,
+                             data={'consumer_key': consumer_key,
+                                   'access_token': access_token,
+                                   'count': count,
+                                   'tag': tag}
+                             )
+    parse = json.loads(response.text)
+    parsed = parse['list']
+    list = "Titles:"
+    for item in parsed:
+        title = parsed[item]['resolved_title'].encode('utf-8')
+        list += "\n" + title
+    return parsed
